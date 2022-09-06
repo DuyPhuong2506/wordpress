db3815b7969a5a4f49411430563e6ae2d3444cd2
Duy Phuong <daop37566@gmail.com>
2022-09-06 14:21:25 +0700

    update

--- wp-config.php
+++ wp-config.php
@@ -20,18 +20,17 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'freedb_database_wp' );
+define( 'DB_NAME', '' );
 
 
-define( 'DB_PORT', '3306' );
 /** Database username */
-define( 'DB_USER', 'freedb_root' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'd&h&!AeMY9zu$ZP' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'sql.freedb.tech' );
+define( 'DB_HOST', '' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
